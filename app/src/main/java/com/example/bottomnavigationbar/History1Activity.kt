77852b5d4abc8f31513f56e8b9c9bcb62db957e9
Ansar747 <celovekfamilia707@gmail.com>
2024-06-01 23:00:35 +0500

    added some new layout for history

---
+++ app/src/main/java/com/example/bottomnavigationbar/History1Activity.kt
@@ -0,0 +1,25 @@
+package com.example.bottomnavigationbar
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class History1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_history1)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val imageView = findViewById<ImageView>(R.id.iv_backspace_history1)
+        imageView.setOnClickListener {
+            finish()
+        }
+    }
+}
--- app/src/main/java/com/example/bottomnavigationbar/HistoryActivity.kt
+++ app/src/main/java/com/example/bottomnavigationbar/HistoryActivity.kt
@@ -1,21 +1,26 @@
 package com.example.bottomnavigationbar
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.bottomnavigationbar.databinding.ActivityHistoryBinding
+import com.example.bottomnavigationbar.databinding.ActivityInteractiveTestBinding
 
 class HistoryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHistoryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_history)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityHistoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.zovstepei.setOnClickListener {
+            val intent = Intent(this@HistoryActivity, History1Activity::class.java)
+            startActivity(intent)
         }
 
         val imageView = findViewById<ImageView>(R.id.iv_backspace_history)
